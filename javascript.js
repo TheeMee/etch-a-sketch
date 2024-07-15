04a1dc738d0ce5949f6187cf754ce88a75e9e40a
Teerath <themetheetath@gmail.com>
2024-07-15 21:05:10 +0700

    created logic for making a canvas using event delegation

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
 // create a square div grid
-console.log('linked');
-gridContainer = document.querySelector('.container')
+gridContainer = document.querySelector('.gridContainer')
 function createGrid (width) {
     for (let i = 0; i < width; i++) {
         const rowContainer = document.createElement('div');
-        rowContainer.classList.add('row-container');
+        rowContainer.classList.add('rowContainer');
         for (let i = 0; i < width; i++) {
             const pixel = document.createElement('pixel');
             pixel.classList.add('pixel')
@@ -13,3 +12,22 @@ function createGrid (width) {
         gridContainer.appendChild(rowContainer);
     }
 }
+
+
+let rowContainer = document.querySelectorAll('.rowContainer');
+let pixel = document.querySelectorAll('.pixel');
+//create a even listener fired when the mouse enters the pixel
+gridContainer.addEventListener('mouseover', (event) => {
+    console.log('log');
+    let target = event.target;
+
+    if (target.classList.contains('rowContainer')) {
+        event.stopPropagation()
+    } else if (target.classList.contains('pixel')) {
+        target.style.backgroundColor = 'red';
+    }
+}); 
+
+createGrid(64);
+
+
