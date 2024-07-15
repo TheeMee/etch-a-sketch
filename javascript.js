e4532225103ce5a018131796cf74e83d397f1729
Teerath <themetheetath@gmail.com>
2024-07-15 21:19:45 +0700

    added logic for clearning canvas and capping number of pixels at 250

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 // create a square div grid
 gridContainer = document.querySelector('.gridContainer')
+
+body = document.querySelector('body');
+
 function createGrid (width) {
     for (let i = 0; i < width; i++) {
         const rowContainer = document.createElement('div');
@@ -16,6 +19,7 @@ function createGrid (width) {
 
 let rowContainer = document.querySelectorAll('.rowContainer');
 let pixel = document.querySelectorAll('.pixel');
+
 //create a even listener fired when the mouse enters the pixel
 gridContainer.addEventListener('mouseover', (event) => {
     console.log('log');
@@ -29,5 +33,27 @@ gridContainer.addEventListener('mouseover', (event) => {
 }); 
 
 createGrid(64);
+
+//logic for cleaning button
+
+const cleaningButton = document.querySelector('#new-button');
+
+cleaningButton.addEventListener('click', () => {
+    do {
+        let width = Number(prompt("please select the width of the square canvas lower than 250"));
+
+        if (width <= 250) {
+            console.log('run');
+            while (gridContainer.lastChild) {
+                gridContainer.removeChild(gridContainer.lastChild);
+            }
+            createGrid(width);
+            break;
+        } else  {
+            alert('please enter a valid number under 250');
+        }
+    } while(true);
+
+})
 
 
